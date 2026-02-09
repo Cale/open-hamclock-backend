51ba1ee43e4ed20b5e41e0d018842e72f77a2f87
Dave Koberstein <davek@komacke.com>
2026-02-08 16:23:21 -0800

    Support docker hub image path names
    Now that we can push to docker hub, we need to support pathnames
    in our images. That means the container and image can't be the same
    name. The path has to be removed.
    
    Change the named to open-hamclock-backend.
    
    Need to change network name because it only supports a small number of
    characters. Just lock that in to ohb which would be similar to the DVC
    name of ohb-htdocs
    
    refactor build script to make it easier to maintain.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,58 +1,95 @@
 #!/bin/bash
 
 # Variables to set
-IMAGE_BASE=hamclock-be
+IMAGE_BASE=komacke/open-hamclock-backend
 VOACAP_VERSION=v.0.7.6
+HTTP_PORT=80
+
+# Don't set anything past here
 TAG=$(git describe --exact-match --tags 2>/dev/null)
 if [ $? -ne 0 ]; then
     echo "Not currently on a tag. Using 'latest'."
     TAG=latest
+    # should we use the git hash?
     #TAG=$(git rev-parse --short HEAD)
 fi
 
-# Don't set anything past here
 IMAGE=$IMAGE_BASE:$TAG
+CONTAINER=${IMAGE_BASE##*/}
 
-# Get our directory locations in order
+# Get our directory locations in figured out
 HERE="$(realpath -s "$(dirname "$0")")"
 THIS="$(basename "$0")"
 cd $HERE
 
-# this hasn't changed since 2020. Also, while we are developing we don't need to keep pulling it.
-if [ ! -e voacap-$VOACAP_VERSION.tgz ]; then
-    curl -s https://codeload.github.com/jawatson/voacapl/tar.gz/refs/tags/v.0.7.6 -o voacap-$VOACAP_VERSION.tgz
-fi
+RETVAL=0
 
-# make the docker-compose file
-sed "s/__IMAGE__/$IMAGE/" docker-compose.yml.tmpl > docker-compose.yml
-sed -i "s/__IMAGE_BASE__/$IMAGE_BASE/" docker-compose.yml
+main() {
+    case $1 in
+        compose)
+            make_docker_compose
+            ;;
+        '')
+            do_all 
+            ;;
+    esac
+}
 
-if $(docker image list --format '{{.Repository}}:{{.Tag}}' | grep -qs $IMAGE) && [ $TAG != latest ]; then
-    echo "The docker image for '$IMAGE' already exists. Please remove it if you want to rebuild."
-    # NOT ENFORCING THIS YET
-    #exit 2
-fi
+do_all() {
+    get_voacap
+    make_docker_compose
+    warn_image_tag
+    build_image
+    done_message
+}
 
-# Build the image
-echo
-echo "Currently building version '$TAG' of '$IMAGE_BASE'"
-pushd "$HERE/.." >/dev/null
-docker build --rm -t $IMAGE -f docker/Dockerfile .
-RETVAL=$?
-popd >/dev/null
+get_voacap() {
+    # this hasn't changed since 2020. Also, while we are developing we don't need to keep pulling it.
+    if [ ! -e voacap-$VOACAP_VERSION.tgz ]; then
+        curl -s https://codeload.github.com/jawatson/voacapl/tar.gz/refs/tags/v.0.7.6 -o voacap-$VOACAP_VERSION.tgz
+    fi
+}
 
-if [ $RETVAL -eq 0 ]; then
-    # basic info
+make_docker_compose() {
+    # make the docker-compose file
+    sed "s|__IMAGE__|$IMAGE|" docker-compose.yml.tmpl > docker-compose.yml
+    sed -i "s/__CONTAINER__/$CONTAINER/" docker-compose.yml
+    sed -i "s/__HTTP_PORT__/$HTTP_PORT/" docker-compose.yml
+}
+
+warn_image_tag() {
+    if $(docker image list --format '{{.Repository}}:{{.Tag}}' | grep -qs $IMAGE) && [ $TAG != latest ]; then
+        echo "The docker image for '$IMAGE' already exists. Please remove it if you want to rebuild."
+        # NOT ENFORCING THIS YET
+        #exit 2
+    fi
+}
+
+build_image() {
+    # Build the image
     echo
-    echo "Completed building '$IMAGE'."
-    echo
-    echo "If this is the first time you are running OHB, run setup first:"
-    echo "    docker-ohb-setup.sh"
-    echo
-    echo "To start the container, launch with docker compose:"
-    echo "    docker compose up -d"
-else
-    echo "build failed with error: $RETVAL"
-fi
+    echo "Currently building version '$TAG' of '$IMAGE_BASE'"
+    pushd "$HERE/.." >/dev/null
+    docker build --rm -t $IMAGE -f docker/Dockerfile .
+    RETVAL=$?
+    popd >/dev/null
+}
 
+done_message() {
+	if [ $RETVAL -eq 0 ]; then
+		# basic info
+		echo
+		echo "Completed building '$IMAGE'."
+		echo
+		echo "If this is the first time you are running OHB, run setup first:"
+		echo "    docker-ohb-setup.sh"
+		echo
+		echo "To start the container, launch with docker compose:"
+		echo "    docker compose up -d"
+	else
+		echo "build failed with error: $RETVAL"
+	fi
+}
+
+main "$@"
 exit $RETVAL
